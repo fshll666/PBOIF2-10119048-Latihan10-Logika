2ffa9d9a6d4bfe918c4452b30e3dab1c8c80a4da
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-17 23:31:01 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan10-Logika/src/pboif2/pkg10119048/latihan10/logika/PBOIF210119048Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * Nama      : Mochammad Faishal
+ * Kelas     : IF2
+ * NIM       : 10119048
+ * Deskripsi : Logika
+ */
+public class PBOIF210119048Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
